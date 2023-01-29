68c95bc26fa945c32bbeb996d3c0e2bc712ba544
Joao Pedro <joaopedrocastro.08@gmail.com>
2023-01-29 16:07:28 -0300

    Update estrutura do projeto

--- CAN_LoopBack/SW4STM32/STM324x9I_EVAL/Debug/Drivers/BSP/Components/ts3510.d
+++
@@ -1,6 +0,0 @@
-Drivers/BSP/Components/ts3510.o: \
- C:/Users/joaop/Documents/Workspace\ STM/Workspace\ Estudo/CAN_LoopBack/Drivers/BSP/Components/ts3510/ts3510.c \
- C:/Users/joaop/Documents/Workspace\ STM/Workspace\ Estudo/CAN_LoopBack/Drivers/BSP/Components/ts3510/ts3510.h \
- c:\users\joaop\documents\workspace\ stm\workspace\ estudo\can_loopback\drivers\bsp\components\common\ts.h
-C:/Users/joaop/Documents/Workspace\ STM/Workspace\ Estudo/CAN_LoopBack/Drivers/BSP/Components/ts3510/ts3510.h:
-c:\users\joaop\documents\workspace\ stm\workspace\ estudo\can_loopback\drivers\bsp\components\common\ts.h:
--- CAN_LoopBack/SW4STM32/STM324x9I_EVAL/Debug/Example/SW4STM32/startup_stm32f429xx.d
+++
@@ -1,2 +0,0 @@
-Example/SW4STM32/startup_stm32f429xx.o: \
- C:/Users/joaop/Documents/Workspace\ STM/Workspace\ Estudo/CAN_LoopBack/SW4STM32/startup_stm32f429xx.s
